8c78b60431517a49bc345612affeb41b073c15c8
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-26 15:28:07 +0100

    new

--- src/components/Responsive.tsx
+++ src/components/Responsive.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AiOutlineClose, AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
 import { BsBuildings, BsInfoLg } from "react-icons/bs";
+import { FaGraduationCap } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 import { GrContact } from "react-icons/gr";
 
@@ -48,7 +49,7 @@ const Responsive = () => {
               className="w-[80%] flex text-black items-center justify-center text-center no-underline rounded-full
              bg-[#87ceeb] p-3 m-3 cursor-pointer hover:scale-110 ease-in duration-200"
             >
-              <AiOutlineHome className="size-6" />
+              <FaGraduationCap className="size-6" />
               <span className="pl-4 font-semibold font-marquis text-[18px]">
                 Program
               </span>
