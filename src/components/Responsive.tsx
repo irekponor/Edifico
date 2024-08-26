b3e0de2daf5ab44a0c3ee4561b5f78badbb14c0e
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-26 12:28:46 +0100

    responsive

--- src/components/Responsive.tsx
+++ src/components/Responsive.tsx
@@ -5,6 +5,7 @@ const Responsive = () => {
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
+    console.log("state change");
   };
   return (
     <div>
@@ -13,10 +14,14 @@ const Responsive = () => {
         className="absolute top-4 right-4 z-[99] md:hidden"
       />
       {nav ? (
-        <div>
-          <a href="">
-            <AiOutlineHome />
-            <span>Home</span>
+        <div className="md:hidden fixed w-full h-screen bg-white flex flex-col justify-center items-center z-20">
+          <a
+            href="#home"
+            className="w-[80%] flex text-black items-center justify-center text-center no-underline rounded-full
+             bg-[#87ceeb] p-4 m-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineHome className="size-6" />
+            <span className="pl-4 font-semibold font-marquis">Home</span>
           </a>
         </div>
       ) : (
