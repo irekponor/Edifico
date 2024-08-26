dac0c126caf092f0b9d3f3a3b255fcd288385c4f
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-26 16:25:37 +0100

    new

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,19 +4,49 @@ const Home = () => {
   return (
     <div>
       <div className="md:block hidden">
-        <header className="fixed top-2 z-[20] mx-auto flex w-full items-center justify-between">
-          <div className="flex">
+        <header className="fixed top-4 z-[20] mx-auto flex w-full items-center justify-between">
+          <div className="flex pl-24">
             <FaUserGraduate className="size-6" />
-            <h3>hsh</h3>
+            <h4 className="font-title pl-2">Edifico</h4>
           </div>
           <div>
-            <div>
-              <a href="">Home</a>
-              <a href="">Info</a>
-              <a href="">Program</a>
-              <a href="">Faculty</a>
-              <a href="">Testimonial</a>
-              <a href="">Contact</a>
+            <div className="mr-12">
+              <a
+                href="#home"
+                className="no-underline p-3 justify-between font-semibold font-title text-[22px]"
+              >
+                Home
+              </a>
+              <a
+                href=""
+                className="no-underline p-3 justify-between font-semibold font-title text-[22px]"
+              >
+                Info
+              </a>
+              <a
+                href=""
+                className="no-underline p-3 justify-between font-semibold font-title text-[22px]"
+              >
+                Program
+              </a>
+              <a
+                href=""
+                className="no-underline p-3 justify-between font-semibold font-title text-[22px]"
+              >
+                Faculty
+              </a>
+              <a
+                href=""
+                className="no-underline p-3 justify-between font-semibold font-title text-[22px]"
+              >
+                Testimonial
+              </a>
+              <a
+                href=""
+                className="no-underline p-3 justify-between font-semibold font-title text-[22px]"
+              >
+                Contact
+              </a>
             </div>
           </div>
         </header>
