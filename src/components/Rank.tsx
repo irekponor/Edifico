a29eec6d52c1cf3672ad5a7b029c328a46219d6d
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-27 03:02:22 +0100

    edited

--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -1,6 +1,6 @@
 const Rank = () => {
   return (
-    <div className="max-w-[1200px] m-auto md:pl-20 p-4 py-16 bg-custom-color">
+    <div className="max-w-[1200px] m-auto md:pl-20 p-4 bg-custom-color">
       <h1 className="text-white font-marquis uppercase text-center text-[18px]">
         Edifico Rankings
       </h1>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,15 +1,48 @@
 import Rank from "./Rank";
+import Testitem from "./Testitem";
 
+const data = [
+  {
+    name: `Anita Adarabioro`,
+    degree: `Bsc, Msc`,
+    img: ``,
+    details:
+      "I am filled with a mix of sadness and gratitude, knowing I will miss the exceptional education and experiences it provided.",
+  },
+  {
+    name: `John statham`,
+    degree: `Bsc, Msc, Phd`,
+    img: ``,
+    details:
+      "The rigorous academic programs at Edufico University prepared me well for my future endeavors, and I am thankful for the opportunity to have studied there.",
+  },
+  {
+    name: `Kofi Kulumaye`,
+    degree: `Phd`,
+    img: ``,
+    details:
+      "Edufico University commitment to excellence and innovation has equipped me with the skills and knowledge to succeed, and I am proud to call myself an alumnus.",
+  },
+  {
+    name: `Toretto Trudeau`,
+    degree: `Msc, Phd`,
+    img: ``,
+    details:
+      "I am reminded of the countless opportunities it provided for growth, exploration, and self-discovery, making my time there truly transformative.",
+  },
+];
 const Testimonials = () => {
   return (
-    <div>
+    <div className="pt-20">
       <Rank />
-      <h1 className="text-custom-color font-marquis uppercase text-center pt-16 text-[19px]">
-        Testimonials
-      </h1>
-      <p className="text-custom-color font-name text-center">
-        What our students say
-      </p>
+      <div className="max-w-[1040px] m-auto md:pl-20 p-4">
+        <h1 className="text-custom-color font-marquis uppercase text-center pt-16 text-[19px]">
+          Testimonials
+        </h1>
+        <p className="text-custom-color font-name text-center">
+          What our students say
+        </p>
+      </div>
     </div>
   );
 };
