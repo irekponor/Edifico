37e2d7ae981558d4a1958e236f6fc5780af5da3e
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-27 05:12:27 +0100

    edited

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,7 @@
 import { BsTwitter } from "react-icons/bs";
 import { FaFacebook, FaLinkedin, FaTiktok } from "react-icons/fa6";
 import { IoLogoYoutube } from "react-icons/io";
+import { MdCopyright } from "react-icons/md";
 import { SiInstagram } from "react-icons/si";
 
 const Contact = () => {
@@ -10,25 +11,75 @@ const Contact = () => {
         <h1 className="font-marquis py-4 text-4xl font-bold text-custom-color">
           Connect with us
         </h1>
-        <a href="">
-          <FaFacebook className="text-black cursor-pointer" size={30} />
+        <a href="" className="flex pt-4">
+          <FaFacebook className="text-custom-color cursor-pointer" size={20} />
         </a>
-        <a href="">
-          <SiInstagram className="text-black cursor-pointer" size={30} />
+        <a href="" className="flex pt-4">
+          <SiInstagram className="text-custom-color cursor-pointer" size={20} />
         </a>
-        <a href="">
-          <FaLinkedin className="text-black cursor-pointer" size={30} />
+        <a href="" className="flex pt-4">
+          <FaLinkedin className="text-custom-color cursor-pointer" size={20} />
         </a>
-        <a href="">
-          <IoLogoYoutube className="text-black cursor-pointer" size={30} />
+        <a href="" className="flex pt-4">
+          <IoLogoYoutube
+            className="text-custom-color cursor-pointer"
+            size={20}
+          />
         </a>
-        <a href="">
-          <BsTwitter className="text-black cursor-pointer" size={30} />
+        <a href="" className="flex pt-4">
+          <BsTwitter className="text-custom-color cursor-pointer" size={20} />
         </a>
-        <a href="">
-          <FaTiktok className="text-black cursor-pointer" size={30} />
+        <a href="" className="flex pt-4">
+          <FaTiktok className="text-custom-color cursor-pointer" size={20} />
         </a>
       </div>
+      <div className="mt-10 pl-28 pr-16 grid gap-16 sm:grid-cols-3">
+        <div>
+          <h3 className="font-marquis text-custom-color mb-3">Quick Links</h3>
+          <p className="font-name text-custom-color">Admission</p>
+          <p className="font-name text-custom-color">Records</p>
+          <p className="font-name text-custom-color">Enterprises</p>
+          <p className="font-name text-custom-color">Staff Intranet</p>
+          <p className="font-name text-custom-color">Support Us</p>
+          <p className="font-name text-custom-color">
+            School Of Advanced Study
+          </p>
+        </div>
+        <div>
+          <h3 className="font-marquis text-custom-color mb-3">Quick Links</h3>
+          <p className="font-name text-custom-color">Admission</p>
+          <p className="font-name text-custom-color">Records</p>
+          <p className="font-name text-custom-color">Enterprises</p>
+          <p className="font-name text-custom-color">Staff Intranet</p>
+          <p className="font-name text-custom-color">Support Us</p>
+          <p className="font-name text-custom-color">
+            School Of Advanced Study
+          </p>
+        </div>
+        <div>
+          <h3 className="font-marquis text-custom-color mb-3">Quick Links</h3>
+          <p className="font-name text-custom-color">Admission</p>
+          <p className="font-name text-custom-color">Records</p>
+          <p className="font-name text-custom-color">Enterprises</p>
+          <p className="font-name text-custom-color">Staff Intranet</p>
+          <p className="font-name text-custom-color">Support Us</p>
+          <p className="font-name text-custom-color">
+            School Of Advanced Study
+          </p>
+        </div>
+      </div>
+      <p className="text-black text-center items-center font-marquis pt-10">
+        <a
+          href=""
+          className="text-black text-center items-center font-marquis pt-10"
+        >
+          <MdCopyright
+            className="text-center items-center text-black"
+            size={20}
+          />
+        </a>
+        2024 Edifico University
+      </p>
     </div>
   );
 };
