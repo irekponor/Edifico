ddaa40ad554ded2d0f8e299b2a83ad16e8b57546
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-27 02:14:57 +0100

    edited

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,6 +4,26 @@ const Testimonials = () => {
       <h1 className="text-white font-marquis uppercase text-center text-[18px]">
         Edifico Rankings
       </h1>
+      <div className="pl-28 pr-16 grid gap-16 sm:grid-cols-4">
+        <div>
+          <h3 className="font-name text-white">#3</h3>
+          <p className="font-name text-white">
+            Top public national Universities
+          </p>
+        </div>
+        <div>
+          <h3 className="font-name text-white">#3</h3>
+          <p>Source: Mmh</p>
+        </div>
+        <div>
+          <h3 className="font-name text-white">#3</h3>
+          <p>Source: Mmh</p>
+        </div>
+        <div>
+          <h3 className="font-name text-white">#3</h3>
+          <p>Source: Mmh</p>
+        </div>
+      </div>
     </div>
   );
 };
