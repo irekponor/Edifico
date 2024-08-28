befe9ff6204e4418b2015987dfd02bee7c1c1d82
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-28 21:58:27 +0100

    font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
-import { MdCopyright } from "react-icons/md";
-
 const Footer = () => {
   return (
-    <div className="bg-custom-color items-center">
-      <h1 className="text-white text-[18px] pb-10 text-center items-center font-marquis">
-        <MdCopyright size={20} /> 2024 Edifico University
-      </h1>
+    <div className="bg-custom-color pt-8">
+      <p className="text-white text-[18px] pb-10 text-center items-center font-marquis">
+        2024 All Rights Reserved
+      </p>
     </div>
   );
 };
