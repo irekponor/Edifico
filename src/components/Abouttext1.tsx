1f15f8d18786c3658cea1564ce561afc16b366e6
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-14 05:04:03 +0100

    about

--- src/components/Abouttext1.tsx
+++ src/components/Abouttext1.tsx
@@ -6,7 +6,13 @@ const Abouttext1 = ({ img, title1 }) => {
         <h3 className="font-name font-semibold text-white text-center pt-3">
           {title1}
         </h3>
-        <p className="font-name pb-0 pt-2 text-white text-center">geee</p>
+        <p className="font-name pb-0 pt-2 text-white text-center">
+          "The Fico Sports Center is our university's athletic hub, featuring
+          state-of-the-art facilities, including basketball courts, soccer
+          fields, and a fitness center. Ranked Best University Sports Facility
+          for July 2024 by Sports Facilities Magazine (SFM), it's the perfect
+          spot for students to play, watch, and cheer on their favorite teams."
+        </p>
       </div>
     </div>
   );
