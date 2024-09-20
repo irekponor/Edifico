2a08df8c57c511ff8d1e7bde19d41dd9f7180f5e
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-20 01:21:37 +0100

    static typing

--- src/components/Abouttext1.tsx
+++ src/components/Abouttext1.tsx
@@ -1,4 +1,9 @@
-const Abouttext1 = ({ img, title1 }) => {
+interface AboutTextProps {
+  img: string;
+  title1: string;
+}
+
+const AboutText: React.FC<AboutTextProps> = ({ img, title1 }) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full rounded-xl group hover:bg-gradient-to-l from-gray-200 to-custom-color">
       <img src={img} alt="/" className="rounded-xl group-hover:opacity-10" />
